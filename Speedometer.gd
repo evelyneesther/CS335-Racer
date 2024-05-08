44e76abc215fefb1a4e4d69b29d0bead8799807c
evelyneesther <eedresner@knox.edu>
2024-05-08 16:02:42 -0500

    Spedometer fixed

--- Speedometer.gd
+++ Speedometer.gd
@@ -1,6 +1,7 @@
 extends TextEdit
 
 var number
+var player
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	number=0
@@ -11,5 +12,7 @@ func _process(delta):
 	self.text=str(number)
 
 func _on_character_body_2d_speed_change(speed):
-	number=int(speed)
+	number=int(speed)/10
 	pass # Replace with function body.
+
+# Replace with function body.
