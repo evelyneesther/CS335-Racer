49e9af266de104a72cb8f4de9c682c455d75f314
James Osborne <jwosborne@knox.edu>
2024-05-08 10:55:38 -0500

    tuned

--- Character.gd
+++ Character.gd
@@ -1,41 +1,42 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-# const JUMP_VELOCITY = 4.5
+const MAX_SPEED = 1000 # pix/sec
+const ACCELERATION_COIFICIENT = 40 # pix/sec^2
+const FRICTION_COIFICTIENT = 10
+const TURN_SPEED = 1.5 # rad/sec
+const MASS = 1
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-# var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var energy = 0 # for accurate physics
+var speed = 0
 
-var forward = false
+# polarity of velocity vector, what turns speed into velocity, stops instant stops on no input
+var polarity = 0
 
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_ESCAPE:
 			get_tree().quit()
-		if event.pressed and event.keycode == KEY_W:
-			forward = true
-		else:
-			forward = false
 
 func _physics_process(delta):
-	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y -= gravity * delta
+	# -x is right, +x left, -y forward, +y backward
+	var direction : Vector2 =  Input.get_vector("forward", "back", "right", "left")
+		
+	# do movement
+	# speed will be 0.0 if direction is not pressed or forward and reverse are pressed at same time
+	if direction.y:
+		speed +=  ACCELERATION_COIFICIENT
+		polarity = direction.y
+	
+	if speed > MAX_SPEED:
+		speed = MAX_SPEED
+	 
+	velocity = self.global_transform.basis.z * speed * polarity
 
-	# Handle jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector( "ui_up", "ui_down", "ui_left", "ui_right")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	# do rotation
+	if direction.y > 0.0:  # if moving backwards
+		rotate_y(TURN_SPEED * -direction.x)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		rotate_y(TURN_SPEED * direction.x)
+		
 	move_and_slide()
--- Character2D.gd
+++ Character2D.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 1000 # pix/sec
-const ACCELERATION_COIFICIENT = 40 # pix/sec^2
-const FRICTION_COIFICTIENT = 10
+const MAX_SPEED = 3000 # pix/sec
+const ACCELERATION_COIFICIENT = 4000 # pix/sec^2
+const FRICTION_COIFICTIENT = 12000
 const TURN_SPEED = 1.5 # rad/sec
 const MASS = 1
 
-var energy = 0 # for accurate physics
+var energy = 0 # for accurate ish physics
 var speed = 0
 
 # polarity of velocity vector, what turns speed into velocity, stops instant stops on no input
@@ -22,22 +22,24 @@ func _physics_process(delta):
 	var rotation_direction = Input.get_axis("left", "right")
 	
 	# Real physics not implemented right, deal with later
-	# energy = ((speed * speed) * MASS) / 2
-	# energy += ACCELERATION_COIFICIENT
-	# speed = sqrt((2 * energy) / MASS)
 	
 	if move_input != 0:
-		speed += ACCELERATION_COIFICIENT
+		energy += ACCELERATION_COIFICIENT
 		polarity = move_input
-	
+		
 	if speed > MAX_SPEED:
-		speed = MAX_SPEED
+		energy = ((MAX_SPEED ** 2) * MASS) / 2
+	
+	speed = sqrt((2 * energy) / MASS)
 	
 	velocity = transform.x * polarity * speed
 	
 	rotation += rotation_direction * TURN_SPEED * delta
 	
-	if speed > 0:
-		speed -= FRICTION_COIFICTIENT 
+	if energy > 0 and not move_input:
+		energy -= FRICTION_COIFICTIENT
+	
+	if energy < 0:
+		energy = 0
 	
 	move_and_slide()
