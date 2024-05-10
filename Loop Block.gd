f05bafe58e2884be0c243dbf84a877a7633350f4
evelyneesther <eedresner@knox.edu>
2024-05-10 15:14:15 -0500

    Re-Added Colisions

--- Loop Block.gd
+++ Loop Block.gd
@@ -1,16 +1,10 @@
 extends StaticBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_loop_detector_player_detected():
+	self.set_collision_layer_value(1, false)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_loop_detector_player_detected():
-	self.set_collision_layer_value(1, false)
+func _on_new_lap_detector_lap_started():
+	self.set_collision_layer_value(1, true)
 	pass # Replace with function body.
