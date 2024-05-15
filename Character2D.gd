f4d323272e8efb3281fda511711235a8b5c9ad73
James Osborne <jwosborne@knox.edu>
2024-05-15 12:27:15 -0500

    basic pseudo-drift added

--- Character2D.gd
+++ Character2D.gd
@@ -7,12 +7,17 @@ const ACCELERATION_COIFICIENT = 6000 # pix/sec^2
 const FRICTION_COIFICTIENT = 20000
 const TURN_SPEED = 2.2 # rad/sec
 const MASS = 1.2
+const DRIFT_MODIFIER = 0.5
 
 var energy = 0 # for accurate ish physics
 var speed = 0
-signal speed_change
+
 # polarity of velocity vector, what turns speed into velocity, stops instant stops on no input
 var polarity = 0
+
+var brake = 0
+
+signal speed_change
 
 func _unhandled_input(event):
 	if event is InputEventKey:
@@ -23,34 +28,37 @@ func _physics_process(delta):
 	var move_input = Input.get_axis("back", "forward")
 	var rotation_direction = Input.get_axis("left", "right")
 	
+	if Input.is_action_just_pressed("brake"):
+		brake = 1
+	elif Input.is_action_just_released("brake"):
+		brake = 0
+	
 	move_input=hitWall(move_input)
 	
 	applyAcceleration(move_input)
 
 	velocity = transform.x * polarity * speed
 	
-	rotation += rotation_direction * TURN_SPEED * delta
+	rotation += rotation_direction * (TURN_SPEED + DRIFT_MODIFIER * brake) * delta
 	
 	applyFriction(move_input)
 	
 	move_and_slide()
-	
-		
 
 func applyAcceleration(move_input):
 	if energy == 0:
 		polarity = move_input
 	
 	if polarity > 0:
-		energy += move_input * ACCELERATION_COIFICIENT
+		energy += move_input * (ACCELERATION_COIFICIENT - brake * 3000)
 	elif polarity < 0:
-		energy += -1 * move_input * ACCELERATION_COIFICIENT
+		energy += -1 * move_input * (ACCELERATION_COIFICIENT + brake * 3000)
 	
 	if energy < 0:
 		energy = 0
 	
-	if speed > MAX_SPEED:
-		energy = ((MAX_SPEED ** 2) * MASS) / 2
+	if speed > (MAX_SPEED - brake * 1000):
+		energy = (((MAX_SPEED - brake * 1000) ** 2) * MASS) / 2
 	
 	speed = sqrt((2 * energy) / MASS)
 	
@@ -83,7 +91,7 @@ func hitWall(moveInput):
 			return(0)
 		
 	return(moveInput)
-	
+
 func printOutputs():
 	print("Speed: ")
 	print(speed)
