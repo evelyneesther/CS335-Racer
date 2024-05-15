733ad5c0e48fb127e805dcd1c28d206e6eec01c6
evelyneesther <eedresner@knox.edu>
2024-05-15 12:44:14 -0500

    added fullscreen mode, got rid of double space

--- CarArt.gd
+++ CarArt.gd
@@ -7,6 +7,15 @@ func _ready():
 	carPath="res://Art/Red/Red -.png"
 	pass # Replace with function body.
 
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.is_action_pressed("fullscreen"):
+			var test=DisplayServer.window_get_mode()
+			if test==0:
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			elif test==3:
+				DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED) 
+			print(test)
 	
 func selected_car():
 	
