770a6b103ad5e04bd8c6dc2bfcf9ac0b8cdc29ab
evelyneesther <eedresner@knox.edu>
2024-05-22 12:39:10 -0500

    Made laps track correctly and not go when they shouldnt, made quit menu function properly

--- Resources/Scripts/Track/overhead.gd
+++ Resources/Scripts/Track/overhead.gd
@@ -8,6 +8,7 @@ extends Node2D
 var first_lap_finished = false
 var laps_completed = 0
 var race_finished = false
+var has_looped=false
 
 
 func _ready():
@@ -17,13 +18,24 @@ func _ready():
 
 
 func _on_new_lap_detector_body_entered(body):
+	print("Has Looped:")
+	print(has_looped)
+	if has_looped==false:
+		return
+	has_looped=false
 	if first_lap_finished:
 		laps_completed += 1
 	else:
 		first_lap_finished = true
+		laps_completed+=1
 	
 	# check laps if max
 	if laps_completed >= ManagerGame.laps_max and race_finished == false:
 		ManagerGame.race_finished.emit()
 		
 		race_finished = true
+
+
+func _on_loop_detector_player_detected():
+	print("Loop Detected, EndZone Enabled")
+	has_looped=true
--- Resources/Scripts/UI/Popups/GameOver.gd
+++ Resources/Scripts/UI/Popups/GameOver.gd
@@ -6,8 +6,10 @@ func _ready():
 
 
 func _on_retry_pressed():
+	get_tree().paused=false
 	get_tree().change_scene_to_file('res://Resources/Scenes/overhead.tscn')
 
 
 func _on_main_menu_pressed():
+	get_tree().paused=false
 	get_tree().change_scene_to_file('res://Resources/Scenes/main_menu.tscn')
