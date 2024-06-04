32c2deb94364fa6c8fd758c39db19cfd7079bd65
evelyneesther <eedresner@knox.edu>
2024-06-04 09:13:31 -0500

    Fixed collision, hopefully actually this time

--- Resources/Scripts/Track/LoopDetector.gd
+++ Resources/Scripts/Track/LoopDetector.gd
@@ -5,4 +5,12 @@ signal player_detected
 
 func _on_input_event(viewport, event, shape_idx):
 	emit_signal("player_detected")
+	
+func emit():
+	emit_signal("player_detected")
+
+
+func _on_body_entered(body):
+	if body==%Vehicle:
+		emit()
 	pass # Replace with function body.
